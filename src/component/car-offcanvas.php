8cc761e047cafa4eace2155d80235e736587e509
Jon desktop <jon@desktop.com>
2024-11-13 16:47:09 -0600

    creamos el componente del carrito offcanvas

---
+++ src/component/car-offcanvas.php
@@ -0,0 +1,15 @@
+    <!-- shop car -->
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="shop" aria-labelledby="offcanvasRightLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasRightLabel">Carrito</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div class="text-center">
+                <img src="carrito-vacio.svg" class="img-fluid d-block m-auto" alt="Carrito vacio">
+                <p class="fs-2 focus">Carrito vacío</p>
+                <p>Agrega productos</p>
+            </div>
+        </div>
+    </div>
+    <!-- end shop car -->
