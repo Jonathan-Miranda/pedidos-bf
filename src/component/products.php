736bec267c95cafe1b499e031a425e0273b5df51
Jon desktop <jon@desktop.com>
2024-11-13 17:40:50 -0600

    creamos el componente de los productos y categorias del home del catalogo

---
+++ src/component/products.php
@@ -0,0 +1,275 @@
+<div class="container">
+  <div class="row">
+    <p class="fs-1 focus mt-3">Productos</p>
+
+    <div class="col-lg-2 col-md-3 mb-3">
+      <form id="filter" class="pegajoso">
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseOne"
+                aria-expanded="false"
+                aria-controls="flush-collapseOne"
+              >
+                Categoria 1
+              </button>
+            </h2>
+            <div
+              id="flush-collapseOne"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value="1"
+                    id="sub-categoria-1"
+                  />
+                  <label class="form-check-label" for="sub-categoria-1"
+                    >sub-categoria-1</label
+                  >
+                </div>
+
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value="2"
+                    id="sub-categoria-2"
+                  />
+                  <label class="form-check-label" for="sub-categoria-2"
+                    >sub-categoria-2</label
+                  >
+                </div>
+
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value="3"
+                    id="sub-categoria-3"
+                  />
+                  <label class="form-check-label" for="sub-categoria-3"
+                    >sub-categoria-3</label
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseTwo"
+                aria-expanded="false"
+                aria-controls="flush-collapseTwo"
+              >
+                Categoria 2
+              </button>
+            </h2>
+            <div
+              id="flush-collapseTwo"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value="1"
+                    id="sub-categoria-1"
+                  />
+                  <label class="form-check-label" for="sub-categoria-1"
+                    >sub-categoria-1</label
+                  >
+                </div>
+
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value="2"
+                    id="sub-categoria-2"
+                  />
+                  <label class="form-check-label" for="sub-categoria-2"
+                    >sub-categoria-2</label
+                  >
+                </div>
+
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value="3"
+                    id="sub-categoria-3"
+                  />
+                  <label class="form-check-label" for="sub-categoria-3"
+                    >sub-categoria-3</label
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseThree"
+                aria-expanded="false"
+                aria-controls="flush-collapseThree"
+              >
+                Categoria 3
+              </button>
+            </h2>
+            <div
+              id="flush-collapseThree"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value="1"
+                    id="sub-categoria-1"
+                  />
+                  <label class="form-check-label" for="sub-categoria-1"
+                    >sub-categoria-1</label
+                  >
+                </div>
+
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value="2"
+                    id="sub-categoria-2"
+                  />
+                  <label class="form-check-label" for="sub-categoria-2"
+                    >sub-categoria-2</label
+                  >
+                </div>
+
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value="3"
+                    id="sub-categoria-3"
+                  />
+                  <label class="form-check-label" for="sub-categoria-3"
+                    >sub-categoria-3</label
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <div class="col-lg-10 col-md-9">
+      <div class="row card-group">
+        <div class="col-lg-3 col-md-4 col-6 mb-3 position-relative">
+          <div class="card" data-bs-toggle="modal" data-bs-target="#prod-md">
+            <img
+              src="https://placehold.co/600"
+              class="card-img-top"
+              alt="..."
+              loading="lazy"
+            />
+            <div class="card-body">
+              <p class="fs-3 focus">Fayrus</p>
+              <p>Amantadina, clorfenamina, paracetamol</p>
+              <span class="fw-light">SKU: 507978</span>
+            </div>
+          </div>
+
+          <div class="z-3 position-absolute top-0 start-0 m-4">
+            <a class="btn rounded-circle btn-danger text-center" href="#">
+              <i class="bi bi-heart"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-6 mb-3 position-relative">
+          <div class="card" data-bs-toggle="modal" data-bs-target="#prod-md">
+            <img
+              src="https://placehold.co/600"
+              class="card-img-top"
+              alt="..."
+              loading="lazy"
+            />
+            <div class="card-body">
+              <p class="fs-3 focus">Fayrus</p>
+              <p>Amantadina, clorfenamina, paracetamol</p>
+              <span class="fw-light">SKU: 507978</span>
+            </div>
+          </div>
+
+          <div class="z-3 position-absolute top-0 start-0 m-4">
+            <a class="btn rounded-circle btn-danger text-center" href="#">
+              <i class="bi bi-heart"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-6 mb-3 position-relative">
+          <div class="card" data-bs-toggle="modal" data-bs-target="#prod-md">
+            <img
+              src="https://placehold.co/600"
+              class="card-img-top"
+              alt="..."
+              loading="lazy"
+            />
+            <div class="card-body">
+              <p class="fs-3 focus">Fayrus</p>
+              <p>Amantadina, clorfenamina, paracetamol</p>
+              <span class="fw-light">SKU: 507978</span>
+            </div>
+          </div>
+
+          <div class="z-3 position-absolute top-0 start-0 m-4">
+            <a class="btn rounded-circle btn-danger text-center" href="#">
+              <i class="bi bi-heart"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-6 mb-3 position-relative">
+          <div class="card" data-bs-toggle="modal" data-bs-target="#prod-md">
+            <img
+              src="https://placehold.co/600"
+              class="card-img-top"
+              alt="..."
+              loading="lazy"
+            />
+            <div class="card-body">
+              <p class="fs-3 focus">Fayrus</p>
+              <p>Amantadina, clorfenamina, paracetamol</p>
+              <span class="fw-light">SKU: 507978</span>
+            </div>
+          </div>
+
+          <div class="z-3 position-absolute top-0 start-0 m-4">
+            <a class="btn rounded-circle btn-danger text-center" href="#">
+              <i class="bi bi-heart"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
