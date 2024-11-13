8f313e300906a7cdcd78b0abe20580fd960cfacd
Jon desktop <jon@desktop.com>
2024-11-13 17:19:52 -0600

    creamos el componente carrucel de productos de casa

---
+++ src/component/carrousel-product-casa.php
@@ -0,0 +1,131 @@
+<!-- home -->
+<div class="container">
+    <div class="row">
+        <p class="fs-1 focus mt-3">Los de casa 🏠
+            <a class="btn rounded-pill bk icon-link icon-link-hover" href="#">
+                Ver todos
+                <svg fill="currentColor" class="bi bi-arrow-right-short fs-4" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                </svg>
+            </a>
+        </p>
+        <div class="col-md-12 mt-3">
+            <!-- Swiper -->
+            <div class="swiper mySwiper">
+                <div class="drag-indicator fs-3"><i class="bi bi-arrows"></i></div>
+                <div class="swiper-wrapper">
+
+                    <div class="swiper-slide">
+                        <div>
+
+                            <img src="slide1.webp" class="radio card-img" alt="...">
+                            <div class="card-img-overlay align-self-end p-3 blur">
+                                <p class="fs-3 focus">Nombre categoria</p>
+                                <p class="">Lorem ipsum dolor sit amet</p>
+                                <a class="btn rounded-pill btn-danger icon-link icon-link-hover" data-bs-toggle="modal" data-bs-target="#prod-md">
+
+                                    Ver producto
+                                    <svg fill="currentColor" class="bi bi-arrow-right-short fs-4" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                    </svg>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="slide2.webp" class="radio card-img" alt="...">
+                            <div class="card-img-overlay align-self-end p-3 blur">
+                                <p class="fs-3 focus">Nombre categoria</p>
+                                <p class="">Lorem ipsum dolor sit amet</p>
+                                <a class="btn rounded-pill btn-danger icon-link icon-link-hover" data-bs-toggle="modal" data-bs-target="#prod-md">
+
+                                    Ver producto
+                                    <svg fill="currentColor" class="bi bi-arrow-right-short fs-4" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="slide3.webp" class="radio card-img" alt="...">
+                            <div class="card-img-overlay align-self-end p-3 blur">
+                                <p class="fs-3 focus">Nombre categoria</p>
+                                <p class="">Lorem ipsum dolor sit amet</p>
+                                <a class="btn rounded-pill btn-danger icon-link icon-link-hover" data-bs-toggle="modal" data-bs-target="#prod-md">
+
+                                    Ver producto
+                                    <svg fill="currentColor" class="bi bi-arrow-right-short fs-4" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="slide1.webp" class="radio card-img" alt="...">
+                            <div class="card-img-overlay align-self-end p-3 blur">
+                                <p class="fs-3 focus">Nombre categoria</p>
+                                <p class="">Lorem ipsum dolor sit amet</p>
+                                <a class="btn rounded-pill btn-danger icon-link icon-link-hover" data-bs-toggle="modal" data-bs-target="#prod-md">
+
+                                    Ver producto
+                                    <svg fill="currentColor" class="bi bi-arrow-right-short fs-4" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="slide2.webp" class="radio card-img" alt="...">
+                            <div class="card-img-overlay align-self-end p-3 blur">
+                                <p class="fs-3 focus">Nombre categoria</p>
+                                <p class="">Lorem ipsum dolor sit amet</p>
+                                <a class="btn rounded-pill btn-danger icon-link icon-link-hover" data-bs-toggle="modal" data-bs-target="#prod-md">
+
+                                    Ver producto
+                                    <svg fill="currentColor" class="bi bi-arrow-right-short fs-4" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="slide3.webp" class="radio card-img" alt="...">
+                            <div class="card-img-overlay align-self-end p-3 blur">
+                                <p class="fs-3 focus">Nombre categoria</p>
+                                <p class="">Lorem ipsum dolor sit amet</p>
+                                <a class="btn rounded-pill btn-danger icon-link icon-link-hover" data-bs-toggle="modal" data-bs-target="#prod-md">
+
+                                    Ver producto
+                                    <svg fill="currentColor" class="bi bi-arrow-right-short fs-4" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+            <!-- End Swiper -->
+        </div>
+    </div>
+</div>
+<!-- end home -->
