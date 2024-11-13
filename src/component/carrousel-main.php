640676a503a9dde9e4617b203a7c92cec1833b7c
Jon desktop <jon@desktop.com>
2024-11-13 17:02:56 -0600

    creamos el componente del carrousel principal del home del catalogo

---
+++ src/component/carrousel-main.php
@@ -0,0 +1,75 @@
+    <!-- carrousel -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="carouselExampleCaptions" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <picture>
+                                <source srcset="https://placehold.co/600x900" media="(max-width: 600px)">
+                                <source srcset="https://placehold.co/800x400" media="(max-width: 1000px)">
+                                <img src="https://placehold.co/1527x400" class="d-block w-100 img-fluid" alt="..." loading="lazy">
+                            </picture>
+                            <div class="carousel-caption">
+                                <a class="btn btn-lg fs-5 rounded-pill btn-danger icon-link icon-link-hover" href="#">
+                                    Ver producto
+                                    <svg fill="currentColor" class="bi bi-arrow-right-short fs-3" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="carousel-item active">
+                            <picture>
+                                <source srcset="https://placehold.co/600x900" media="(max-width: 600px)">
+                                <source srcset="https://placehold.co/800x400" media="(max-width: 1000px)">
+                                <img src="https://placehold.co/1527x400" class="d-block w-100 img-fluid" alt="..." loading="lazy">
+                            </picture>
+                            <div class="carousel-caption">
+                                <a class="btn btn-lg fs-5 rounded-pill btn-danger icon-link icon-link-hover" href="#">
+                                    Ver producto
+                                    <svg fill="currentColor" class="bi bi-arrow-right-short fs-3" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="carousel-item active">
+                            <picture>
+                                <source srcset="https://placehold.co/600x900" media="(max-width: 600px)">
+                                <source srcset="https://placehold.co/800x400" media="(max-width: 1000px)">
+                                <img src="https://placehold.co/1527x400" class="d-block w-100 img-fluid" alt="..." loading="lazy">
+                            </picture>
+                            <div class="carousel-caption">
+                                <a class="btn btn-lg fs-5 rounded-pill btn-danger icon-link icon-link-hover" href="#">
+                                    Ver producto
+                                    <svg fill="currentColor" class="bi bi-arrow-right-short fs-3" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- end carrousel -->
