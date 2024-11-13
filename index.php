189d195a51b6dbfbd4b6e1ce871055aaf22fdaea
Jon desktop <jon@desktop.com>
2024-11-13 16:07:39 -0600

    Agregamos los estilos propios, bootstrap y fuentes google al login del sistema

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <?php
+    require('src/component/bootstrap.php');
+    ?>
 </head>
 
 <body>
