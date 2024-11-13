81d09a91eba633e5d716186ae3c0d913d45605f7
Jon desktop <jon@desktop.com>
2024-11-13 16:53:08 -0600

    creamos el componente de lista de deseos offcanvas

---
+++ src/component/like-offcanvas.php
@@ -0,0 +1,15 @@
+ <!-- like -->
+ <div class="offcanvas offcanvas-end" tabindex="-1" id="like" aria-labelledby="offcanvasRightLabel">
+     <div class="offcanvas-header">
+         <h5 class="offcanvas-title" id="offcanvasRightLabel">Favoritos</h5>
+         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+     </div>
+     <div class="offcanvas-body">
+         <div class="text-center">
+             <img src="favorito-vacio.svg" width="80px" height="auto" class="img-fluid d-block m-auto" alt="Sin favoritos">
+             <p class="fs-2 focus">Aun no tienes favoritos</p>
+             <p>-¡Pss! los mejores son los de casa.😉</p>
+         </div>
+     </div>
+ </div>
+ <!-- end like -->
