45d6f90745926aa9938fb1c3e6a3f44ffa3eab47
Jon desktop <jon@desktop.com>
2024-11-14 11:05:29 -0600

    agregamos el js del carrousel a la vista del producto

--- src/views/product.php
+++ src/views/product.php
@@ -29,6 +29,10 @@
     ?>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
+    <?php
+    require('js/swiper-carrousel-related.php');
+    ?>
+
 </body>
 
 </html>
