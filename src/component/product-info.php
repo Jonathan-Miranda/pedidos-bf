b9947b7268bc1007b6812dc5f73a15cac0496e68
Jon desktop <jon@desktop.com>
2024-11-14 11:09:50 -0600

    creamos el componente informacion de vista producto

---
+++ src/component/product-info.php
@@ -0,0 +1,165 @@
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-md-5">
+            <img class="img-fluid d-block m-auto" src="https://placehold.co/600" alt="Producto" loading="lazy">
+        </div>
+        <div class="col-md-7">
+            <p class="fs-2 focus">Nombre producto</p>
+            <p>Sales</p>
+            <p>Presentación</p>
+            <p>Categoria</p>
+            <p>sku: 506398</p>
+            <p>Registro sanitario: 456M2001SSAIV</p>
+
+            <div class="input-group">
+                <button class="btn bk" type="button" id="button-addon1"><i class="bi bi-dash"></i></button>
+                <input type="number" class="form-control" id="inputGroupFile04">
+                <button class="btn bk" type="button" id="inputGroupFileAddon04"><i class="bi bi-plus"></i></button>
+            </div>
+
+            <div class="d-grid  mt-3 gap-2">
+                <button class="btn btn-purple" type="button">Agregar</button>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <p class="fs-2 focus mt-3">Te podria interesar</p>
+
+        <div class="col-md-12 mt-3">
+            <!-- Swiper -->
+            <div class="swiper mySwiper2">
+                <div class="drag-indicator2 fs-3"><i class="bi bi-arrows"></i></div>
+                <div class="swiper-wrapper">
+
+                    <div class="swiper-slide position-relative">
+
+                        <div class="card">
+
+                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
+                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
+                                <p class="fs-3 focus">Fayrus-1</p>
+                                <p>Amantadina, clorfenamina, paracetamol</p>
+                                <span class="fw-light">SKU: 507978</span>
+                            </div>
+                        </div>
+
+                        <div class="z-3 position-absolute top-0 start-0 m-3">
+                            <a class="btn rounded-circle btn-danger text-center" href="#">
+                                <i class="bi bi-heart-fill"></i>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="swiper-slide position-relative">
+
+                        <div class="card">
+
+                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
+                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
+                                <p class="fs-3 focus">Fayrus-2</p>
+                                <p>Amantadina, clorfenamina, paracetamol</p>
+                                <span class="fw-light">SKU: 507978</span>
+                            </div>
+                        </div>
+
+                        <div class="z-3 position-absolute top-0 start-0 m-3">
+                            <a class="btn rounded-circle btn-danger text-center" href="#">
+                                <i class="bi bi-heart-fill"></i>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="swiper-slide position-relative">
+
+                        <div class="card">
+
+                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
+                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
+                                <p class="fs-3 focus">Fayrus-3</p>
+                                <p>Amantadina, clorfenamina, paracetamol</p>
+                                <span class="fw-light">SKU: 507978</span>
+                            </div>
+                        </div>
+
+                        <div class="z-3 position-absolute top-0 start-0 m-3">
+                            <a class="btn rounded-circle btn-danger text-center" href="#">
+                                <i class="bi bi-heart-fill"></i>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="swiper-slide position-relative">
+
+                        <div class="card">
+
+                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
+                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
+                                <p class="fs-3 focus">Fayrus-4</p>
+                                <p>Amantadina, clorfenamina, paracetamol</p>
+                                <span class="fw-light">SKU: 507978</span>
+                            </div>
+                        </div>
+
+                        <div class="z-3 position-absolute top-0 start-0 m-3">
+                            <a class="btn rounded-circle btn-danger text-center" href="#">
+                                <i class="bi bi-heart-fill"></i>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="swiper-slide position-relative">
+
+                        <div class="card">
+
+                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
+                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
+                                <p class="fs-3 focus">Fayrus-5</p>
+                                <p>Amantadina, clorfenamina, paracetamol</p>
+                                <span class="fw-light">SKU: 507978</span>
+                            </div>
+                        </div>
+
+                        <div class="z-3 position-absolute top-0 start-0 m-3">
+                            <a class="btn rounded-circle btn-danger text-center" href="#">
+                                <i class="bi bi-heart-fill"></i>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="swiper-slide position-relative">
+
+                        <div class="card">
+
+                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
+                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
+                                <p class="fs-3 focus">Fayrus-6</p>
+                                <p>Amantadina, clorfenamina, paracetamol</p>
+                                <span class="fw-light">SKU: 507978</span>
+                            </div>
+                        </div>
+
+                        <div class="z-3 position-absolute top-0 start-0 m-3">
+                            <a class="btn rounded-circle btn-danger text-center" href="#">
+                                <i class="bi bi-heart-fill"></i>
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- End Swiper -->
+
+        </div>
+    </div>
+
+</div>
