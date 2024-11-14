a4562b63cee13f68377ad6ab756a338610b52430
Jon desktop <jon@desktop.com>
2024-11-14 11:20:10 -0600

    agregamos el componente de informacion a la vista de producto

--- src/views/product.php
+++ src/views/product.php
@@ -25,6 +25,10 @@
     ?>
 
     <?php
+    require('src/component/product-info.php');
+    ?>
+
+    <?php
     require('src/component/jquery-bootstrap.php');
     ?>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
