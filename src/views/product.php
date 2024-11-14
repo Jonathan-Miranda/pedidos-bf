3314677c1aa29752b9c2bf6cc3c53244eebc5237
Jon desktop <jon@desktop.com>
2024-11-14 11:01:48 -0600

    creamos el componente vista producto con sus dependencias css y js necesarias, estructura del menu y componentes

---
+++ src/views/product.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Producto | Brudifarma</title>
+    <?php
+    require('src/component/bootstrap.php');
+    ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
+
+<body>
+    <?php
+    require('src/component/navbar.php');
+    ?>
+
+    <?php
+    require('src/component/car-offcanvas.php');
+    ?>
+
+    <?php
+    require('src/component/like-offcanvas.php');
+    ?>
+
+    <?php
+    require('src/component/jquery-bootstrap.php');
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+</body>
+
+</html>
