f99db14b9874a6e256c26a9d4d0daaafd18d13e5
Jon laptop <jon@laptop.com>
2024-11-29 17:25:26 -0600

    vaceamos en el carrousel la data de productos favoritos, usamos una sola consulta tanto para el offcanvas como para el carrusel para tener una mejor eficiencia, eliminamos el btn de favorito en la card y una alerta si el user no tiene favoritos

--- src/component/carrousel-like-prod.php
+++ src/component/carrousel-like-prod.php
@@ -5,7 +5,8 @@
             <a class="btn rounded-pill bk icon-link icon-link-hover" href="#">
                 Ver todos
                 <svg fill="currentColor" class="bi bi-arrow-right-short fs-4" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                    <path fill-rule="evenodd"
+                        d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
                 </svg>
             </a>
         </p>
@@ -15,127 +16,32 @@
             <div class="swiper mySwiper2">
                 <div class="drag-indicator2 fs-3"><i class="bi bi-arrows"></i></div>
                 <div class="swiper-wrapper">
+                    <?php
+                    if (count($wishListItems) > 0) {
+                        foreach ($wishListItems as $row_wish) {
+                            ?>
 
-                    <div class="swiper-slide position-relative">
-
-                        <div class="card">
-
-                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
-                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
-                                <p class="fs-3 focus">Fayrus-1</p>
-                                <p>Amantadina, clorfenamina, paracetamol</p>
-                                <span class="fw-light">SKU: 507978</span>
+                            <div class="swiper-slide">
+                                <div class="card">
+                                    <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy" />
+                                    <div class="card-body">
+                                        <p class="fs-5 focus"><?php echo $row_wish['NOMBRE']; ?></p>
+                                        <p><?php echo $row_wish['DESCRIPCION']; ?></p>
+                                        <span class="fw-light">SKU: <?php echo $row_wish['SKU']; ?></span>
+                                    </div>
+                                </div>
                             </div>
+
+                            <?php
+                        }
+                    } else {
+                        ?>
+                        <div class="alert alert-warning" role="alert">
+                            🥺 Aun no tienes productos favoritos
                         </div>
-
-                        <div class="z-3 position-absolute top-0 start-0 m-3">
-                            <a class="btn rounded-circle btn-danger text-center" href="#">
-                                <i class="bi bi-heart-fill"></i>
-                            </a>
-                        </div>
-
-                    </div>
-
-                    <div class="swiper-slide position-relative">
-
-                        <div class="card">
-
-                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
-                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
-                                <p class="fs-3 focus">Fayrus-2</p>
-                                <p>Amantadina, clorfenamina, paracetamol</p>
-                                <span class="fw-light">SKU: 507978</span>
-                            </div>
-                        </div>
-
-                        <div class="z-3 position-absolute top-0 start-0 m-3">
-                            <a class="btn rounded-circle btn-danger text-center" href="#">
-                                <i class="bi bi-heart-fill"></i>
-                            </a>
-                        </div>
-
-                    </div>
-
-                    <div class="swiper-slide position-relative">
-
-                        <div class="card">
-
-                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
-                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
-                                <p class="fs-3 focus">Fayrus-3</p>
-                                <p>Amantadina, clorfenamina, paracetamol</p>
-                                <span class="fw-light">SKU: 507978</span>
-                            </div>
-                        </div>
-
-                        <div class="z-3 position-absolute top-0 start-0 m-3">
-                            <a class="btn rounded-circle btn-danger text-center" href="#">
-                                <i class="bi bi-heart-fill"></i>
-                            </a>
-                        </div>
-
-                    </div>
-
-                    <div class="swiper-slide position-relative">
-
-                        <div class="card">
-
-                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
-                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
-                                <p class="fs-3 focus">Fayrus-4</p>
-                                <p>Amantadina, clorfenamina, paracetamol</p>
-                                <span class="fw-light">SKU: 507978</span>
-                            </div>
-                        </div>
-
-                        <div class="z-3 position-absolute top-0 start-0 m-3">
-                            <a class="btn rounded-circle btn-danger text-center" href="#">
-                                <i class="bi bi-heart-fill"></i>
-                            </a>
-                        </div>
-
-                    </div>
-
-                    <div class="swiper-slide position-relative">
-
-                        <div class="card">
-
-                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
-                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
-                                <p class="fs-3 focus">Fayrus-5</p>
-                                <p>Amantadina, clorfenamina, paracetamol</p>
-                                <span class="fw-light">SKU: 507978</span>
-                            </div>
-                        </div>
-
-                        <div class="z-3 position-absolute top-0 start-0 m-3">
-                            <a class="btn rounded-circle btn-danger text-center" href="#">
-                                <i class="bi bi-heart-fill"></i>
-                            </a>
-                        </div>
-
-                    </div>
-
-                    <div class="swiper-slide position-relative">
-
-                        <div class="card">
-
-                            <img src="https://placehold.co/600" class="card-img-top" alt="..." loading="lazy">
-                            <div class="card-body" data-bs-toggle="modal" data-bs-target="#prod-md">
-                                <p class="fs-3 focus">Fayrus-6</p>
-                                <p>Amantadina, clorfenamina, paracetamol</p>
-                                <span class="fw-light">SKU: 507978</span>
-                            </div>
-                        </div>
-
-                        <div class="z-3 position-absolute top-0 start-0 m-3">
-                            <a class="btn rounded-circle btn-danger text-center" href="#">
-                                <i class="bi bi-heart-fill"></i>
-                            </a>
-                        </div>
-
-                    </div>
-
+                        <?php
+                    }
+                    ?>
                 </div>
 
             </div>
